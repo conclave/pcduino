1ab1e63e3897639ef32652814d09ff2a843e36e0
Markus Tzoe <teamo@live.com>
2014-08-18 20:25:21 +0800

    add sample/sonar

---
+++ sample/sonar/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+
+	. "github.com/conclave/pcduino/hardware/core"
+)
+
+const (
+	SPEED_OF_SOUND float64 = 343.0 / 2 / 10000
+)
+
+var trig byte
+var echo byte
+
+func init() {
+	Init()
+	flag.Parse()
+	if flag.NArg() < 2 {
+		fmt.Fprintf(os.Stderr, "invoke: %v\n", os.ErrInvalid)
+		os.Exit(-1)
+	}
+	trig_, _ := strconv.Atoi(flag.Arg(0))
+	echo_, _ := strconv.Atoi(flag.Arg(1))
+	trig = byte(trig_)
+	echo = byte(echo_)
+	PinMode(echo, INPUT)
+	PinMode(trig, OUTPUT)
+	DigitalWrite(trig, LOW)
+	Delay(20)
+}
+
+func loop() {
+	DigitalWrite(trig, HIGH)
+	DelayMicrosends(20)
+	DigitalWrite(trig, LOW)
+	duration := PulseIn(echo, HIGH, 1000000)
+	fmt.Printf("cm: %.2f\n", float64(duration)*SPEED_OF_SOUND)
+	Delay(80)
+}
+
+func main() {
+	for {
+		loop()
+	}
+}
