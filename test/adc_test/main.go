663361061eab9f6cfc4427ba359362bc5ee55b9e
Markus Tzoe <teamo@live.com>
2014-08-19 22:12:22 +0800

    add test skeleton

---
+++ test/adc_test/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+
+	. "github.com/conclave/pcduino/core"
+)
+
+func init() {
+	Init()
+	setup()
+}
+
+func main() {
+	for {
+		loop()
+	}
+}
+
+var adc_id int = 0
+
+// impl your own setup() and loop()
+func setup() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		fmt.Printf("Usage: program ADC_ID(0/1/2/3/4/5/6/7/8/9/10/11)\n")
+		fmt.Println("Default will get ADC0 value")
+	}
+	adc_id, _ = strconv.Atoi(flag.Arg(0))
+}
+
+func loop() {
+	value := AnalogRead(byte(adc_id))
+	fmt.Printf("ADC%d level is %d\n", adc_id, value)
+	DelayMicrosends(100000)
+}
