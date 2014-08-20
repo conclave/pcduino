8317afaa35fa6022bee2777eefe881abebf94b17
Markus Tzoe <teamo@live.com>
2014-08-20 14:25:07 +0800

    update test/linker_joystick_test

--- test/linker_joystick_test/main.go
+++ test/linker_joystick_test/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	. "github.com/conclave/pcduino/core"
 )
 
@@ -16,8 +18,13 @@ func main() {
 }
 
 func setup() {
+	println("Joystick Test Code!")
+	println("Using ADC_2 and ADC_3.")
 }
 
 func loop() {
-	Delay(100)
+	sensorValue := AnalogRead(2)
+	sensorValue2 := AnalogRead(3)
+	fmt.Printf("The X and Y coordinate is:%d x %d\n", sensorValue, sensorValue2)
+	Delay(500)
 }
