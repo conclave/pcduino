e0b9d97d064d8cb11b02bf3ecdae228538d3de7b
Markus Tzoe <teamo@live.com>
2014-08-20 11:35:05 +0800

    update test/linker_button_test

--- test/linker_button_test/main.go
+++ test/linker_button_test/main.go
@@ -15,9 +15,18 @@ func main() {
 	}
 }
 
+var btnPin byte = 1
+var ledPin byte = 0
+
 func setup() {
+	println("Butten Test Code!")
+	println("Using I/O_0=Drive LED and I/O_1=Button input.")
+	PinMode(ledPin, OUTPUT)
+	PinMode(btnPin, INPUT)
 }
 
 func loop() {
-	Delay(100)
+	btnIn := DigitalRead(btnPin)
+	DigitalWrite(ledPin, btnIn)
+	Delay(10)
 }
