a2eeecac76e81ceb9c633b1a112b4df45b87140a
Markus Tzoe <teamo@live.com>
2014-08-20 14:14:33 +0800

    update test/linker_hall_sensor_test

--- test/linker_hall_sensor_test/main.go
+++ test/linker_hall_sensor_test/main.go
@@ -15,9 +15,17 @@ func main() {
 	}
 }
 
+var sensorPin byte = 2
+var ledPin byte = 0
+
 func setup() {
+	println("Hall sensor test code!")
+	println("Using I/O_0=Drive LED, I/O_2=Sensor output.")
+	PinMode(sensorPin, INPUT)
+	PinMode(ledPin, OUTPUT)
 }
 
 func loop() {
-	Delay(100)
+	value := DigitalRead(sensorPin)
+	DigitalWrite(ledPin, value)
 }
