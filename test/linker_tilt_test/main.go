e1b9bc213f8f782eda8f4fdb5ac2b9380c0cafc7
Markus Tzoe <teamo@live.com>
2014-08-20 15:37:46 +0800

    update test/linker_tilt_test

--- test/linker_tilt_test/main.go
+++ test/linker_tilt_test/main.go
@@ -15,9 +15,22 @@ func main() {
 	}
 }
 
+var ledPin byte = 0
+var switchPin byte = 1
+var val byte
+
 func setup() {
+	println("Tilt sensor test code!")
+	println("Using I/O_0=Drive LED, I/O_1=Sensor output.")
+	PinMode(ledPin, OUTPUT)
+	PinMode(switchPin, INPUT)
 }
 
 func loop() {
-	Delay(100)
+	val = DigitalRead(switchPin)
+	if val == HIGH {
+		DigitalWrite(ledPin, HIGH)
+	} else {
+		DigitalWrite(ledPin, LOW)
+	}
 }
