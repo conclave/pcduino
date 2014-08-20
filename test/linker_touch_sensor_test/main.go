5874d8d5e4e109510d115f3f831d2c417e2dda42
Markus Tzoe <teamo@live.com>
2014-08-20 15:39:44 +0800

    update test/linker_touch_sensor_test

--- test/linker_touch_sensor_test/main.go
+++ test/linker_touch_sensor_test/main.go
@@ -15,9 +15,17 @@ func main() {
 	}
 }
 
+var touchPin byte = 1
+var ledPin byte = 0
+
 func setup() {
+	println("Touch sensor test code!")
+	println("Using I/O_0=Drive LED, I/O_1=Sensor output.")
+	PinMode(touchPin, INPUT)
+	PinMode(ledPin, OUTPUT)
 }
 
 func loop() {
-	Delay(100)
+	val := DigitalRead(touchPin)
+	DigitalWrite(ledPin, val)
 }
