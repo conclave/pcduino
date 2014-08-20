4043175072a6797286c8a80bd968c74b6d9c6ada
Markus Tzoe <teamo@live.com>
2014-08-20 15:05:27 +0800

    update test/linker_relay_test

--- test/linker_relay_test/main.go
+++ test/linker_relay_test/main.go
@@ -15,9 +15,17 @@ func main() {
 	}
 }
 
+var pin byte = 0
+
 func setup() {
+	println("Relay test code!")
+	println("Using I/O_0.")
+	PinMode(pin, OUTPUT)
 }
 
 func loop() {
-	Delay(100)
+	DigitalWrite(pin, HIGH)
+	Delay(5000)
+	DigitalWrite(pin, LOW)
+	Delay(5000)
 }
