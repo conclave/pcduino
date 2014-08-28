1a6e07b9a6ba4eceecf4e4ad2bac03121ad63829
Markus Tzoe <teamo@live.com>
2014-08-28 10:48:15 +0800

    suppose to impl test/pn532readMifareMemory

--- test/pn532readMifareMemory/main.go
+++ test/pn532readMifareMemory/main.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	. "github.com/conclave/pcduino/core"
+	"github.com/conclave/pcduino/module/pn532"
 )
+
+const (
+	SCK  = 13
+	MOSI = 11
+	SS   = 10
+	MISO = 12
+)
+
+var nfc *pn532.PN532
 
 func init() {
 	Init()
@@ -16,8 +29,29 @@ func main() {
 }
 
 func setup() {
+	nfc = pn532.New(SCK, MISO, MOSI, SS)
+	version := nfc.GetFirmwareVersion()
+	if version == 0 {
+		fmt.Fprintln(os.Stderr, "PN53x board not found")
+		os.Exit(-1)
+	}
+	nfc.SAMConfig() // configure board to read RFID tags and cards
+	fmt.Printf("Found chip PN5: %x\n", byte((version>>24)&0xFF))
+	fmt.Printf("Firmware ver. %d.%d\n", byte((version>>16)&0xFF), byte((version>>8)&0xFF))
+	fmt.Printf("Supports: %d\n", byte(version&0xFF))
 }
 
 func loop() {
-	Delay(100)
+	id := nfc.ReadPassiveTargetID(pn532.PN532_MIFARE_ISO14443A)
+	if id != 0 {
+		fmt.Printf("Read card #%d\n", id)
+		keys := []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
+		if nfc.AuthenticateBlock(1, byte(id), 0x08, pn532.KEY_A, keys) { //authenticate block 0x08
+			block := make([]byte, 16)
+			if nfc.ReadMemoryBlock(1, 0x08, block) {
+				fmt.Printf("Read block_0x08: %x\n", block)
+			}
+		}
+	}
+	Delay(500)
 }
