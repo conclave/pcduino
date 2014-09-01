54300bdf7d56fc5277b58ae6e5ea3d8e9abd073b
Markus Tzoe <teamo@live.com>
2014-09-01 19:00:59 +0800

    update core/serial

--- core/serial.go
+++ core/serial.go
@@ -1,3 +1,5 @@
+// +build linux,!cgo
+
 package core
 
 import (
@@ -32,11 +34,9 @@ func (this *hwSerial) Begin(baud uint, config byte) error {
 	}
 	Hw_PinMode(GPIO0, IO_UART_FUNC)
 	Hw_PinMode(GPIO1, IO_UART_FUNC)
-	// set attribute
+
 	rate := uint32(get_valid_baud(baud))
-	if rate == 0 {
-		return nil
-	}
+	fd := uintptr(this.file.Fd())
 	t := syscall.Termios{
 		Iflag:  syscall.IGNPAR,
 		Cflag:  syscall.CS8 | syscall.CREAD | syscall.CLOCAL | rate,
@@ -44,19 +44,32 @@ func (this *hwSerial) Begin(baud uint, config byte) error {
 		Ispeed: rate,
 		Ospeed: rate,
 	}
-	fd := this.file.Fd()
-	if _, _, errno := syscall.Syscall6(
-		syscall.SYS_IOCTL,
-		uintptr(fd),
-		uintptr(syscall.TCSETS),
-		uintptr(unsafe.Pointer(&t)),
-		0,
-		0,
-		0,
-	); errno != 0 {
-		return errno
+	t.Cflag &= ^syscall.CSIZE
+	t.Cflag |= uint32(get_databit(config))
+
+	switch get_parity(config) {
+	case 'O':
+		t.Cflag |= (syscall.PARODD | syscall.PARENB)
+		t.Iflag |= syscall.INPCK
+	case 'E':
+		t.Cflag |= syscall.PARENB
+		t.Cflag &= ^syscall.PARODD
+		t.Iflag |= syscall.INPCK
+	default:
+		t.Cflag &= ^syscall.PARENB
+		t.Iflag &= ^syscall.INPCK
 	}
-	if err := syscall.SetNonblock(int(fd), false); err != nil {
+
+	switch get_stopbit(config) {
+	case 2:
+		t.Cflag |= syscall.CSTOPB
+	default:
+		t.Cflag &= ^syscall.CSTOPB
+	}
+
+	t.Cflag &= ^(syscall.ICANON | syscall.ECHO | syscall.ECHOE | syscall.ISIG)
+	err := Ioctl(fd, uintptr(syscall.TCSETS), uintptr(unsafe.Pointer(&t)))
+	if err != nil {
 		return err
 	}
 	return nil
@@ -104,16 +117,16 @@ const (
 func get_databit(config byte) byte {
 	switch config {
 	case SERIAL_5N1, SERIAL_5N2, SERIAL_5E1, SERIAL_5E2, SERIAL_5O1, SERIAL_5O2:
-		return _CS5
+		return syscall.CS5
 	case SERIAL_6N1, SERIAL_6N2, SERIAL_6E1, SERIAL_6E2, SERIAL_6O1, SERIAL_6O2:
-		return _CS6
+		return syscall.CS6
 	case SERIAL_7N1, SERIAL_7N2, SERIAL_7E1, SERIAL_7E2, SERIAL_7O1, SERIAL_7O2:
-		return _CS7
+		return syscall.CS7
 		// case SERIAL_8N1, SERIAL_8N2, SERIAL_8E1, SERIAL_8E2, SERIAL_8O1, SERIAL_8O2:
 		// default:
 		// 	return CS8
 	}
-	return _CS8
+	return syscall.CS8
 }
 
 func get_stopbit(config byte) byte {
@@ -143,68 +156,25 @@ func get_parity(config byte) byte {
 func get_valid_baud(speed uint) uint {
 	switch speed {
 	case 300:
-		return _B300
+		return syscall.B300
 	case 600:
-		return _B600
+		return syscall.B600
 	case 1200:
-		return _B1200
+		return syscall.B1200
 	case 2400:
-		return _B2400
+		return syscall.B2400
 	case 4800:
-		return _B4800
+		return syscall.B4800
 	case 9600:
-		return _B9600
-	case 14400:
-		return 0
+		return syscall.B9600
 	case 19200:
-		return _B19200
-	case 28800:
-		return 0
+		return syscall.B19200
 	case 38400:
-		return _B38400
+		return syscall.B38400
 	case 57600:
-		return _B57600
+		return syscall.B57600
 	case 115200:
-		return _B115200
+		return syscall.B115200
 	}
 	return 0
 }
-
-const ( // from /usr/include/x86_64-linux-gnu/bits/termios.h
-	_CS5 = 0000000
-	_CS6 = 0000020
-	_CS7 = 0000040
-	_CS8 = 0000060
-
-	_B0       = 0000000 /* hang up */
-	_B50      = 0000001
-	_B75      = 0000002
-	_B110     = 0000003
-	_B134     = 0000004
-	_B150     = 0000005
-	_B200     = 0000006
-	_B300     = 0000007
-	_B600     = 0000010
-	_B1200    = 0000011
-	_B1800    = 0000012
-	_B2400    = 0000013
-	_B4800    = 0000014
-	_B9600    = 0000015
-	_B19200   = 0000016
-	_B38400   = 0000017
-	_B57600   = 0010001
-	_B115200  = 0010002
-	_B230400  = 0010003
-	_B460800  = 0010004
-	_B500000  = 0010005
-	_B576000  = 0010006
-	_B921600  = 0010007
-	_B1000000 = 0010010
-	_B1152000 = 0010011
-	_B1500000 = 0010012
-	_B2000000 = 0010013
-	_B2500000 = 0010014
-	_B3000000 = 0010015
-	_B3500000 = 0010016
-	_B4000000 = 0010017
-)
